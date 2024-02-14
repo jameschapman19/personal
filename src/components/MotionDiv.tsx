54ef5cc14037c2c745e2ef0d7ba68fe43aadc0a0
jameschapman19 <james.chapman.19@ucl.ac.uk>
2024-02-14 21:31:55 +0000

    OO nice update!

--- src/components/MotionDiv.tsx
+++ src/components/MotionDiv.tsx
@@ -1,15 +1,19 @@
-// components/MotionDiv.tsx
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const MotionDiv = ({ children, direction = 'right' }) => {
+interface MotionDivProps {
+    children: React.ReactNode; // Explicitly typing the children prop
+    direction?: 'left' | 'right' | 'bottom'; // You can also explicitly define the possible directions
+}
+
+const MotionDiv: React.FC<MotionDivProps> = ({ children, direction = 'right' }) => {
     const variants = {
-        hidden: { opacity: 0, x: direction === 'right' ? 50 : direction === 'left' ? 50 : 0, y: direction === 'bottom' ? 50 : 0 },
+        hidden: { opacity: 0, x: direction === 'right' ? 50 : direction === 'left' ? -50 : 0, y: direction === 'bottom' ? 50 : 0 },
         visible: {
             opacity: 1,
             x: 0,
             y: 0,
-            transition: { delay: 1.0, duration: 1 }
+            transition: { delay: 0.5, duration: 1 }
         }
     };
 
